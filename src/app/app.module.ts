fb850325532c2a33365215c17f29a49190975b80
TimSwerts <de.swerts@hotmail.com>
2018-11-19 16:05:53 +0100

    Overzetting naar ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatSidenavModule, MatGridListModule } from '@angular/material';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { RoutingModule } from './routing/routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, LeaderboardComponent, MainContentComponent],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatToolbarModule,
-    MatSidenavModule,
     RoutingModule,
-    MatGridListModule
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
