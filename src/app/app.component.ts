ecb2a5ca7400c654b3bc624ded31dfe6693dc08b
TimSwerts <de.swerts@hotmail.com>
2018-11-19 11:30:42 +0100

    Header aangemaakt  en  material angular toegevoegd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+})
+
+@NgModule({
+  imports:[]
 })
 export class AppComponent {
   title = 'app';
