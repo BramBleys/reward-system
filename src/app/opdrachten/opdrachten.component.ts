88b64ffad6302c01240ccfa24ed03b9a8c43db49
robbert van hove <robbertvanhove1@gmail.com>
2018-11-21 14:04:25 +0100

    paginatie opdrachten in orde gebracht

--- src/app/opdrachten/opdrachten.component.ts
+++ src/app/opdrachten/opdrachten.component.ts
@@ -11,16 +11,47 @@ export class OpdrachtenComponent implements OnInit {
 
   allAsignments: Opdracht;
 
-  currentPage = 1;
+  opdrachten$: Observable<any[]>;
+  count$: Observable<Number>;
+  filterParams = {
+    offset: 0,
+    limit: 5,
+    sortBy: 'titel',
+    order: 'asc'
+  };
+
+  readonly pageSize = 5; 
+  
+  private _currentPage = 1;
+  get currentPage(){
+    return this._currentPage;
+  }
+  set currentPage(page){
+    this._currentPage = page;
+    this.filterParams['offset'] = this._currentPage * this.pageSize - this.pageSize;
+    this.getAssignmentsFiltered(this.filterParams);
+  }
+
+  
+
 
   constructor(public opdrachtService: OpdrachtService) {
   }
 
   ngOnInit() {
-    this.getAllAssignments();
+    this.getAssignmentsFiltered(this.filterParams);
+    this.getAssignementsCount(this.filterParams);
   }
 
-  getAllAssignments() {
-     this.opdrachtService.getAllAssignments().subscribe((data) => (this.allAsignments = data));
+
+  getAssignmentsFiltered(filter = {}){
+
+    this.opdrachten$ = this.opdrachtService.getAssignmentsFiltered(filter);
+    this.opdrachten$.subscribe(e => console.log(e));
+   
   }
+  getAssignementsCount(filter = {}){
+    this.count$ = this.opdrachtService.getCount(filter);
+  }
+  
 }
