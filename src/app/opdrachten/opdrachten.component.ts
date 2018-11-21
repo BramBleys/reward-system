dafe77081263604fbdaf7554f0d0f2ab085ec221
robbert van hove <robbertvanhove1@gmail.com>
2018-11-21 11:20:44 +0100

    na merge 2

--- src/app/opdrachten/opdrachten.component.ts
+++ src/app/opdrachten/opdrachten.component.ts
@@ -24,17 +24,3 @@ export class OpdrachtenComponent implements OnInit {
      this.opdrachtService.getAllAssignments().subscribe((data) => (this.allAsignments = data));
   }
 }
-<<<<<<< HEAD
-
-
-
-// const headers = new HttpHeaders();
-// headers.set('token', 'eyJhbGciOiJIUzI1NiJ9.NWJmMjlkNjQ5ZjFhOGM1NWE0ODNhMThk.CdXb4Bmuuk21XvMZiGNxaDcd8R39rohT--9rzhT1P9Y');
-//
-// this.http.get('http://localhost:3000/v1/opdrachten', {
-//   naam: 'ding',
-//   punten: 100,
-//   beschikbaar: true
-// }, {headers} ).subscribe(e => console.log(e));
-=======
->>>>>>> 52fd8175f5cd8ad2b2e46f9c9f5d9b41c6122a11
