e5661231f04853e41aa75b2e921df97d0ba61c1f
TimSwerts <de.swerts@hotmail.com>
2018-11-22 18:02:09 +0100

    type service + class toegevoegd

---
+++ src/app/services/types.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Type } from '../models/type';
+import { Observable } from 'rxjs';
+import { environment as api } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RewardsService {
+  private url = 'https://radiant-peak-48979.herokuapp.com/v1/types';
+
+  constructor(private http: HttpClient) {}
+
+  getRewards(): Observable<Type[]> {
+    const headers = new HttpHeaders();
+    headers.set(
+      'token',
+      'eyJhbGciOiJIUzI1NiJ9.NWJmMmExZDg1OTQyNDYzODZjYmYyNDY4.9fUrbPXXOAuU9n-9l3Ot5GnhQB2bguyfXOX82IP0Olg'
+    );
+
+    return this.http.get<any>(this.url, { headers });
+  }
+}
