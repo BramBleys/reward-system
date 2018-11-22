36695c684d3611e95e0e43d5a230bb0461914ec0
Bram Van Bergen <r0656050@student.thomasmore.be>
2018-11-22 18:01:46 +0100

    Fixed last merge errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,6 @@ import { SidebarAdminComponent } from './sidebar-admin/sidebar-admin.component';
 import { OpdrachtenModule } from './opdrachten/opdrachten.module';
 import { OpdrachtenComponent } from './opdrachten/opdrachten.component';
 import { RewardsCrudComponent } from './rewards-crud/rewards-crud.component';
-import { FormsModule } from '@angular/forms';
 import { TranslateService } from './services/translate.service';
 import { TranslatePipe } from './translate.pipe';
 import {FilterPipe} from './pipes/filterPipe';
@@ -30,11 +29,13 @@ import {FilterPipe} from './pipes/filterPipe';
     HeaderComponent,
     LeaderboardComponent,
     MainContentComponent,
-<<<<<<< HEAD
     LoginComponent,
     SidebarScoresComponent,
     SidebarAdminComponent,
-    OpdrachtenComponent
+    OpdrachtenComponent,
+    RewardsCrudComponent,
+    TranslatePipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
@@ -46,18 +47,7 @@ import {FilterPipe} from './pipes/filterPipe';
     NgbModule.forRoot(),
     OpdrachtenModule
   ],
-  providers: [],
-=======
-    SidebarScoresComponent,
-    SidebarAdminComponent,
-    OpdrachtenComponent,
-    RewardsCrudComponent,
-    TranslatePipe,
-    FilterPipe
-  ],
-  imports: [BrowserModule, RoutingModule, HttpClientModule, NgbModule.forRoot(), OpdrachtenModule, FormsModule],
   providers: [TranslateService],
->>>>>>> 0e8d8f595c663047f8fc54ad6e2a6b04f7a2c1a8
   bootstrap: [AppComponent]
 })
 export class AppModule { }
