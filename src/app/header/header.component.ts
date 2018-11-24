03093f0bd3494872cd4ccffe5a1a7f7c23f7d0aa
TimSwerts <de.swerts@hotmail.com>
2018-11-24 18:25:34 +0100

    +

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,28 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../services/auth.service';
-import { User } from '../models/user';
-import { TranslateService } from '../services/translate.service';
-import { NgbDropdownConfig } from '@ng-bootstrap/ng-bootstrap';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../services/auth.service";
+import { User } from "../models/user";
+import { TranslateService } from "../services/translate.service";
+import { NgbDropdownConfig } from "@ng-bootstrap/ng-bootstrap";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"]
 })
 export class HeaderComponent implements OnInit {
   currentuser: User;
   loggedIn = false;
   admin = false;
-  logintext = 'Log in';
+  logintext = "Log in";
   isCollapsed = true;
 
-  constructor(public authService: AuthService, private translate: TranslateService, config: NgbDropdownConfig, private router: Router) {
-    config.placement = 'bottom-right';
+  constructor(
+    public authService: AuthService,
+    private translate: TranslateService,
+    config: NgbDropdownConfig,
+    private router: Router
+  ) {
+    config.placement = "bottom-right";
     config.autoClose = false;
   }
-
-
 
   ngOnInit() {
     this.authService.userData$.subscribe(user => {
@@ -33,10 +36,11 @@ export class HeaderComponent implements OnInit {
       } else {
         this.loggedIn = false;
       }
-    }
-
-    logout() {
-      this.authService.logout();
-      window.location.reload();
-    }
+    });
   }
+  
+  logout() {
+    this.authService.logout();
+    window.location.reload();
+  }
+}
